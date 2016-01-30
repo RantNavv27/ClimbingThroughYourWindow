256aa9d95e63eeaa3b12ac97d3d27f59ade432ee
RantNavv <daniel@scidice.co.uk>
2016-01-30 01:57:59 +0000

    Cleaned up Player code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,63 +13,26 @@ public class Player : MonoBehaviour
 	Vector3 slowdown = transform.forward;
 	slowdown *= back + forward;
 	GetComponent<Rigidbody>().velocity = slowdown;
+
 	forward -= 0.2f;
 	back += 0.2f;
 	left += 0.1f;
 	right -= 0.1f;
 
+	if (Input.GetKey (KeyCode.W)) forward += 0.5f;
+	if (Input.GetKey (KeyCode.S)) back -= 0.5f;
+	if (Input.GetKey (KeyCode.A)) left = -0.3f;
+	if (Input.GetKey (KeyCode.D)) right = 0.3f;
 
-	if (Input.GetKey (KeyCode.W)) {
-
-		forward += 0.5f;
-		GetComponent<Rigidbody>().AddForce (transform.forward * forward);
-
-	}
-	if (Input.GetKey (KeyCode.S)) {
-
-		back -= 0.5f;
-		GetComponent<Rigidbody>().AddForce (transform.forward * back);
-
-	}
-	if (Input.GetKey (KeyCode.A)) {
-
-		left = -0.3f;
-		Invoke("TurnSlowDown", 1f);
-		//forward -= 0.32f;
-	} else if (Input.GetKey (KeyCode.D)) {
-
-		right = 0.3f;
-		Invoke("TurnSlowDown", 1f);
-		//forward -= 0.32f;
-	}
-
-	if (forward >= 10.0f) {
-		forward = 10.0f;
-	}
-	if (forward <= 0.0f) {
-		forward = 0.0f;
-	}
-	if (back <= -7.0f) {
-		back = -7.0f;
-	}
-	if (back >= 0.0f) back = 0.0f;
-	if (right >= 0.3f) {
-		right = 0.3f;
-	}
-	if (right <= 0.0f) {
-		right = 0.0f;
-	}
-	if (left <= -0.3f) {
-		left = -0.3f;
-	}
-	if (left >= 0.0f) {
-		left = 0.0f;
-	}
+	forward = Mathf.Clamp (forward,0, 10);
+	back = Mathf.Clamp (back,-7, 0);
+	left = Mathf.Clamp (left,-0.3f, 0f);
+	right = Mathf.Clamp (right,0, 0.3f);
 
 	transform.Rotate (0, left * forward, 0);
 	transform.Rotate (0, right * forward, 0);
 	transform.Rotate (0, left * back, 0);
 	transform.Rotate (0, right * back, 0);
 
-}
+	}
 }
